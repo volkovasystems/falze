3b10a38d107ee46d31b255cfc364c7a5cea61286
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-10 16:33:33 +0800

    Updated test.

--- falze-test.js
+++ falze-test.js
@@ -3,3 +3,6 @@
 const falze = require( "./falze.js" );
 
 console.log( falze( { } ) );
+console.log( falze( [ ] ) );
+console.log( falze( { "name": "simple" } ) );
+console.log( falze( [ 1, 2, 3 ] ) );
