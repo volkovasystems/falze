edb273b28d891e939116e083d190d5e08f9227f9
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 19:23:01 +0800

    Updated test.

--- falze-test.js
+++ falze-test.js
@@ -1,19 +1,19 @@
 const assert = require( "assert" );
 const falze = require( "./falze.js" );
 
-assert.equal( falze( NaN ), true, "should be true" );
+assert.equal( falze( NaN ), true, "should be equal to true" );
 
-assert.equal( falze( null ), true, "should be true" );
+assert.equal( falze( null ), true, "should be equal to true" );
 
-assert.equal( falze( undefined ), true, "should be true" );
+assert.equal( falze( undefined ), true, "should be equal to true" );
 
-assert.equal( falze( ), true, "should be true" );
+assert.equal( falze( ), true, "should be equal to true" );
 
-assert.equal( falze( "" ), true, "should be true" );
+assert.equal( falze( "" ), true, "should be equal to true" );
 
-assert.equal( falze( { } ), true, "should be true" );
+assert.equal( falze( { } ), true, "should be equal to true" );
 
-assert.equal( falze( [ ] ), true, "should be true" );
+assert.equal( falze( [ ] ), true, "should be equal to true" );
 
 class ClassA{
 	constructor( ){ }
@@ -24,21 +24,21 @@ let testA = new ClassA( );
 assert.equal( falze( testA ), true, "should return true" );
 
 
-assert.equal( falze( "hello" ), false, "should be false" );
+assert.equal( falze( "hello" ), false, "should be equal to false" );
 
-assert.equal( falze( { "name": "simple" } ), false, "should be false" );
+assert.equal( falze( { "name": "simple" } ), false, "should be equal to false" );
 
-assert.equal( falze( [ 1, 2, 3 ] ), false, "should be false" );
+assert.equal( falze( [ 1, 2, 3 ] ), false, "should be equal to false" );
 
-assert.equal( falze( 0 ), false, "should be false" );
+assert.equal( falze( 0 ), false, "should be equal to false" );
 
 assert.equal( falze( Symbol.for( "hello" ) ), false, "should return false" );
 
 assert.equal( falze( Error ), false, "should return false" );
 
-assert.equal( falze( function hello( ){ } ), false, "should be false" );
+assert.equal( falze( function hello( ){ } ), false, "should be equal to false" );
 
-assert.equal( falze( Infinity ), false, "should be false" );
+assert.equal( falze( Infinity ), false, "should be equal to false" );
 
 assert.equal( falze( ( ) => { } ), false, "should return false" );
 
